566686be64bd4dd73be026fa1217a31d5175c27e
정남진 <randomuser14123@yu.ac.kr>
2025-07-21 17:21:21 +0900

    chore/spring boot init
    Jira : S13P11D103-138

---
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/AmrBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.amr_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmrBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AmrBackendApplication>(*args)
+}
