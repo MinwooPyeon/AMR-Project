281b1e7d0682bd012ee5d53c0ee8eff1ecda8ca2
박주현 <yeochi1201@gmail.com>
2025-07-26 21:35:23 +0900

    Revert "Merge branch 'feature/Additional_Unity_Setting' into 'master'"
    This reverts merge request !16

--- Android/app/src/main/java/com/android/ssamr/main/SSAMRApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.android.ssamr.main
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SSAMRApplication : Application()
--- Backend/amr_backend/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "amr_backend"
