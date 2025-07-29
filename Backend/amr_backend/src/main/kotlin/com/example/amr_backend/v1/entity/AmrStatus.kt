32e3ac1b8b6a844f633f9cddb852cf596ad27a6e
정남진 <randomuser14123@yu.ac.kr>
2025-07-29 14:10:43 +0900

    feat/define amr status entity
    Jira : S13P11D103-212

---
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/entity/AmrStatus.kt
@@ -0,0 +1,68 @@
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.annotation.CreatedDate
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "amr_status")
+data class AmrStatus(
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    val id: Long,
+
+    @Column(name = "name")
+    val name: String,
+
+    @Column(name = "status")
+    val status: String,
+
+    @Column(name = "battery_level")
+    val batteryLevel: Int,
+
+    @Column(name = "x")
+    val x: Double,
+
+    @Column(name = "y")
+    val y: Double,
+
+    @Column(name = "speed")
+    val speed: Double,
+
+    @Column(name = "serial")
+    val serial: String,
+
+    @Column(name = "firmware_version")
+    val firmwareVersion: String,
+
+    @Column(name = "last_update_date")
+    val lastUpdateDate: LocalDateTime,
+
+    @CreatedDate
+    val createdAt: LocalDateTime
+) {
+    final override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        val oEffectiveClass =
+            if (other is HibernateProxy) other.hibernateLazyInitializer.persistentClass else other.javaClass
+        val thisEffectiveClass =
+            if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
+        if (thisEffectiveClass != oEffectiveClass) return false
+        other as AmrStatus
+
+        return id == other.id
+    }
+
+    final override fun hashCode(): Int =
+        if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass.hashCode() else javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , name = $name , status = $status , batteryLevel = $batteryLevel , x = $x , y = $y , speed = $speed , serial = $serial , firmwareVersion = $firmwareVersion , lastUpdateDate = $lastUpdateDate )"
+    }
+}
