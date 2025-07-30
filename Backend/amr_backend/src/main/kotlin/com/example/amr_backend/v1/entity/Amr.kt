e8e3d6588d7b4e13287c789c2dbb729569d4e8d6
정남진 <randomuser14123@yu.ac.kr>
2025-07-30 10:08:27 +0900

    feat/define Amr entity and update AmrStatus to include it

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/entity/Amr.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/entity/Amr.kt
@@ -2,50 +2,36 @@ package com.example.amr_backend.v1.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.hibernate.proxy.HibernateProxy
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity
-@EntityListeners(AuditingEntityListener::class)
-@Table(name = "amr_status")
-data class AmrStatus(
+@Table(name = "amr")
+data class Amr(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     val id: Long = 0L,
 
-    @ManyToOne
-    @JoinColumn(name = "amr_serial", referencedColumnName = "serial", nullable = false)
-    val amr: Amr,
+    @Column(name = "name")
+    val name: String,
 
-    @Column(name = "status")
-    val status: String,
+    @Column(name = "mqtt_url", nullable = false)
+    val mqttUrl: String,
 
-    @Column(name = "battery_level")
-    val batteryLevel: Int,
+    @Column(name = "serial", unique = true, nullable = false)
+    val serial: String,
 
-    @Column(name = "x")
-    val x: Double,
+    @Column(name = "firmware_version")
+    val firmwareVersion: String,
 
-    @Column(name = "y")
-    val y: Double,
-
-    @Column(name = "speed")
-    val speed: Double,
+    @Column(name = "last_update_date")
+    val lastUpdateDate: LocalDateTime,
 ) {
-    @CreatedDate
-    @Column(name = "created_at")
-    lateinit var createdAt: LocalDateTime
-
     final override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null) return false
@@ -54,7 +40,7 @@ data class AmrStatus(
         val thisEffectiveClass =
             if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
         if (thisEffectiveClass != oEffectiveClass) return false
-        other as AmrStatus
+        other as Amr
 
         return id == other.id
     }
@@ -64,7 +50,7 @@ data class AmrStatus(
 
     @Override
     override fun toString(): String {
-        return this::class.simpleName + "(id = $id , amr = $amr , status = $status , batteryLevel = $batteryLevel , x = $x , y = $y , speed = $speed , createdAt = $createdAt )"
+        return this::class.simpleName + "(id = $id , name = $name , mqttUrl = $mqttUrl , serial = $serial , firmwareVersion = $firmwareVersion , lastUpdateDate = $lastUpdateDate )"
     }
 
 }
