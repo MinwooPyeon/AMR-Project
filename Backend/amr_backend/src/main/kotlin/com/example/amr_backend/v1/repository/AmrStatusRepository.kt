329e81a87dbc528aaf7de8bddaf9850c5b303a4e
정남진 <randomuser14123@yu.ac.kr>
2025-07-30 16:42:30 +0900

    feat/implement getting the latest status of AMRs
    used native query option for performance
    
    Jira : S13P11D103-217

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/repository/AmrStatusRepository.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/repository/AmrStatusRepository.kt
@@ -2,6 +2,9 @@ package com.example.amr_backend.v1.repository
 
 import com.example.amr_backend.v1.entity.AmrStatus
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface AmrStatusRepository : JpaRepository<AmrStatus, Long> {
+    @Query("SELECT DISTINCT ON (amr_serial) * FROM amr_status ORDER BY amr_serial, created_at DESC", nativeQuery = true)
+    fun findAllLatestStatuses(): List<AmrStatus>
 }
