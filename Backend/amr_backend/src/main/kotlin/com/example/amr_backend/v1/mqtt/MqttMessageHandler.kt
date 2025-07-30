6c83c0764df23805f62b426916d110db8f1ccbb0
정남진 <randomuser14123@yu.ac.kr>
2025-07-30 09:53:25 +0900

    feat/add exception handling

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/mqtt/MqttMessageHandler.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/mqtt/MqttMessageHandler.kt
@@ -5,6 +5,7 @@ import com.example.amr_backend.v1.repository.AmrStatusRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener
 import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,10 +14,15 @@ class MqttMessageHandler(
     private val amrStatusRepository: AmrStatusRepository,
     private val objectMapper: ObjectMapper,
 ) : IMqttMessageListener {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @Transactional
     override fun messageArrived(topic: String?, message: MqttMessage?) {
-        val payloadContents = if (message != null) String(message.payload) else null
-        val amrStatus = objectMapper.readValue(payloadContents, AmrStatus::class.java)
-        amrStatusRepository.save(amrStatus)
+        try {
+            val amrStatus = objectMapper.readValue(message?.payload, AmrStatus::class.java)
+            amrStatusRepository.save(amrStatus)
+        } catch (e: Exception) {
+            logger.warn("Failed to handle a message : {}", e.message)
+        }
     }
 }
