0dab85ba15a86f07855ce149e79fb07d860c6880
정남진 <randomuser14123@yu.ac.kr>
2025-07-30 14:22:42 +0900

    feat/change mqtt messege type

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/mqtt/MqttMessageHandler.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/mqtt/MqttMessageHandler.kt
@@ -1,6 +1,8 @@
 package com.example.amr_backend.v1.mqtt
 
-import com.example.amr_backend.v1.entity.AmrStatus
+import com.example.amr_backend.v1.dto.AmrStatusMessage
+import com.example.amr_backend.v1.dto.toEntity
+import com.example.amr_backend.v1.repository.AmrRepository
 import com.example.amr_backend.v1.repository.AmrStatusRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional
 @Component
 class MqttMessageHandler(
     private val amrStatusRepository: AmrStatusRepository,
+    private val amrRepository: AmrRepository,
     private val objectMapper: ObjectMapper,
 ) : IMqttMessageListener {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -19,8 +22,9 @@ class MqttMessageHandler(
     @Transactional
     override fun messageArrived(topic: String?, message: MqttMessage?) {
         try {
-            val amrStatus = objectMapper.readValue(message?.payload, AmrStatus::class.java)
-            amrStatusRepository.save(amrStatus)
+            val amrStatusMessage = objectMapper.readValue(message?.payload, AmrStatusMessage::class.java)
+            val amr = amrRepository.findBySerial(amrStatusMessage.serial)
+            amrStatusRepository.save(amrStatusMessage.toEntity(amr))
         } catch (e: Exception) {
             logger.warn("Failed to handle a message : {}", e.message)
         }
