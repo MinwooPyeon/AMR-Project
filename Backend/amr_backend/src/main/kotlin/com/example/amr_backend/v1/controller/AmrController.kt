6744bda646904a1cdbd8e905260e49ead61d4ce2
정남진 <randomuser14123@yu.ac.kr>
2025-08-01 14:27:29 +0900

    feat/create endpoint to get detail AMR information
    Jira : S13P11D103-234

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/controller/AmrController.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/controller/AmrController.kt
@@ -1,18 +1,23 @@
 package com.example.amr_backend.v1.controller
 
 import com.example.amr_backend.v1.dto.AmrStatusResponse
+import com.example.amr_backend.v1.dto.toAmrDetailResponse
 import com.example.amr_backend.v1.dto.toResponse
-import com.example.amr_backend.v1.repository.AmrStatusRepository
+import com.example.amr_backend.v1.service.AmrService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/amrs")
 class AmrController(
-    private val amrStatusRepository: AmrStatusRepository
+    private val amrService: AmrService
 ) {
     @GetMapping("/latest-statuses")
     fun findAllLatestStatuses(): List<AmrStatusResponse> =
-        amrStatusRepository.findAllLatestStatuses().map { it.toResponse() }
+        amrService.findAllLatestStatuses().map { it.toResponse() }
+
+    @GetMapping("/detail/{id}")
+    fun findAmrDetailById(@PathVariable id: Long) = amrService.findAmrDetail(id).toAmrDetailResponse()
 }
