05a0801d13f286e7234640dfca243c8aff1af0d9
손병하 <lastadvance@naver.com>
2025-08-01 17:28:16 +0900

    update/detail

--- Android/app/src/main/java/com/android/ssamr/core/data/remote/service/AmrService.kt
+++ Android/app/src/main/java/com/android/ssamr/core/data/remote/service/AmrService.kt
@@ -4,22 +4,21 @@ import com.android.ssamr.core.data.model.amr.response.AmrDetailDto
 import com.android.ssamr.core.data.model.amr.response.AmrDto
 import com.android.ssamr.core.network.model.ApiResponse
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 import retrofit2.Response
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AmrService {
-    @GET("/amr/list")
+    @GET("api/v1/amr/list")
     suspend fun getAmrList(): ApiResponse<List<AmrDto>>
 
-    @GET("/amr/detail")
-    suspend fun getAmrDetail(@Query("id") amrId: Long): ApiResponse<AmrDetailDto>
+    @GET("api/v1/amr/detail")
+    suspend fun getAmrDetail(@Path("id") amrId: Long): ApiResponse<AmrDetailDto>
 
-    @POST("amr/{id}/manual-start")
+    @POST("api/v1/amr/{id}/manual-start")
     suspend fun requestManualStart(@Path("id") id: Long): Response<Unit>
 
-    @POST("amr/{id}/manual-return")
+    @POST("api/v1/amr/{id}/manual-return")
     suspend fun requestManualReturn(@Path("id") id: Long): Response<Unit>
 }
