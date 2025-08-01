f7fa786fc3c326b32ce8aaf1024e572405a35a74
정남진 <randomuser14123@yu.ac.kr>
2025-08-01 15:25:07 +0900

    refactor/change manual control message format

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/dto/AmrManualControlMessage.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/dto/AmrManualControlMessage.kt
@@ -1,12 +1,6 @@
 package com.example.amr_backend.v1.dto
 
-import org.springframework.boot.actuate.endpoint.OperationType
-
 data class AmrManualControlMessage(
     val serial: String,
-    val operationType: OperationType,
+    val area: String,
 )
-
-enum class OperationType {
-    MOVE_FORWARD, MOVE_BACKWARD, TURN_RIGHT, TURN_LEFT
-}
