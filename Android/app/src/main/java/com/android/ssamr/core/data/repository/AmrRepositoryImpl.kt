8f50bc8c4a85f7af31e360fa73051ff921c83079
박상윤 <park48569803@gmail.com>
2025-08-07 09:27:45 +0900

    feat/Webcam server streaming connect
    Webcam server streaming connect
    
    Jira: S13P11D103-197 S13P11D103-198

--- Android/app/src/main/java/com/android/ssamr/core/data/repository/AmrRepositoryImpl.kt
+++ Android/app/src/main/java/com/android/ssamr/core/data/repository/AmrRepositoryImpl.kt
@@ -14,15 +14,15 @@ class AmrRepositoryImpl @Inject constructor(
 
     override suspend fun getAmrList(): List<AmrStatus> {
         return listOf(
-            AmrStatus(1L, "1번로봇", AmrAction.RUNNING, "A-1구역", "1.4m/s", "자재이동"),
-            AmrStatus(2L, "2번로봇", AmrAction.CHARGING, "충전소", "0.0m/s", "대기"),
-            AmrStatus(3L, "3번로봇", AmrAction.CHECKING, "B-3구역", "0.0m/s", "점검")
+            AmrStatus(1L, "1번로봇", AmrAction.RUNNING, 0.0, 1.0, "1.4m/s", "자재이동"),
+            AmrStatus(2L, "2번로봇", AmrAction.CHARGING, 2.0, 3.0, "0.0m/s", "대기"),
+            AmrStatus(3L, "3번로봇", AmrAction.CHECKING, 4.0, 5.0, "0.0m/s", "점검")
         )
 //        return remoteDataSource.getAmrList()
     }
 
     override suspend fun getAmrDetail(amrId: Long): AmrDetailStatus {
-        return AmrDetailStatus("AMR-001", AmrDetailAction.RUNNING, "A-1구역", "1.4m/s", "자재이동", "RB-100", "RB100-2024-001", "v2.1.3", "111.111.1111")
+        return AmrDetailStatus(1,"AMR-001", AmrDetailAction.RUNNING, 0.0, 1.0, "1.4m/s", "자재이동", "RB-100", "RB100-2024-001", "v2.1.3", "111.111.1111")
 //        return remoteDataSource.getAmrDetail(amrId)
     }
 
--- Android/app/src/main/java/com/android/ssamr/feature/amrWebcam/AmrWebcamScreen.kt
+++ Android/app/src/main/java/com/android/ssamr/feature/amrWebcam/AmrWebcamScreen.kt
@@ -1,42 +1,112 @@
 package com.android.ssamr.feature.amrWebcam
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.android.ssamr.R
 import com.android.ssamr.ui.theme.SSAMRTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun AmrWebcamScreen(
     state: AmrWebcamState,
-    sendIntent: (AmrWebcamIntent) -> Unit
+    sendIntent: (AmrWebcamIntent) -> Unit,
+    onFullScreenChanged: (Boolean) -> Unit = {},
 ) {
+    val context = LocalContext.current
+    val activity = context as? Activity
+
+    var isFullScreen by remember { mutableStateOf(false) }
+    val systemUiController = rememberSystemUiController()
+
+    // 전체화면 상태가 바뀔 때만 orientation을 바꿈
+    LaunchedEffect(isFullScreen) {
+        onFullScreenChanged(isFullScreen)
+        if (isFullScreen) {
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            systemUiController.isSystemBarsVisible = false
+            Log.d("WEB", "isFullScreen = $isFullScreen")
+        } else {
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+            systemUiController.isSystemBarsVisible = true
+            Log.d("WEB", "isFullScreen = $isFullScreen")
+        }
+    }
+
+    // 이 route가 완전히 dispose될 때만 orientation을 원복
+    DisposableEffect(Unit) {
+        Log.d("WEB", "DisposableEffect enter")
+        onDispose {
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+            systemUiController.isSystemBarsVisible = true
+            Log.d("WEB", "DisposableEffect dispose")
+        }
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
         RtspPlayerView(
             url = state.rtspUrl,
             modifier = Modifier.fillMaxSize()
         )
 
-        AmrWebcamInfoPanel(
-            state = state,
+        if (!isFullScreen) {
+            AmrWebcamInfoPanel(
+                state = state,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(16.dp)
+            )
+        }
+        IconButton(
+            onClick = {
+//                onToggleFullScreen?.invoke()
+                isFullScreen = !isFullScreen
+                      },
             modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(16.dp)
-        )
+                .align(Alignment.TopEnd)
+                .padding(20.dp)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_fullscreen),
+                contentDescription = if (isFullScreen) "전체화면 종료" else "전체화면",
+                tint = androidx.compose.ui.graphics.Color.White
+            )
+        }
     }
 }
+
 
 @Preview
 @Composable
 fun ArmWebcamScreenPreview() {
     SSAMRTheme {
         AmrWebcamScreen(
-            state = AmrWebcamState()
-
-        ) { }
+            state = AmrWebcamState(),
+            sendIntent = {},
+        )
     }
 }
