9603104599ece9c8ffd88a3864cecae5d72442d5
정남진 <randomuser14123@yu.ac.kr>
2025-08-12 16:21:34 +0900

    feat/create endpoint to update isRead of Notification
    Jira : S13P11D103-377

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/controller/NotificationController.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/controller/NotificationController.kt
@@ -1,11 +1,14 @@
 package com.example.amr_backend.v1.controller
 
+import com.example.amr_backend.v1.dto.NotificationReadUpdateRequest
 import com.example.amr_backend.v1.dto.NotificationResponse
 import com.example.amr_backend.v1.dto.toNotificationResponse
 import com.example.amr_backend.v1.entity.Notification
 import com.example.amr_backend.v1.service.NotificationService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,4 +24,11 @@ class NotificationController(
     @GetMapping("/{id}")
     fun getById(@PathVariable id: Long): NotificationResponse =
         notificationService.getById(id).toNotificationResponse()
+
+    @PatchMapping("/{id}")
+    fun updateRead(
+        @PathVariable id: Long,
+        @RequestBody readUpdateRequest: NotificationReadUpdateRequest,
+    ): NotificationResponse =
+        notificationService.updateRead(id, readUpdateRequest.isRead).toNotificationResponse()
 }
---
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/dto/NotificationReadUpdateRequest.kt
@@ -0,0 +1,5 @@
+package com.example.amr_backend.v1.dto
+
+data class NotificationReadUpdateRequest(
+    val isRead: Boolean
+)
