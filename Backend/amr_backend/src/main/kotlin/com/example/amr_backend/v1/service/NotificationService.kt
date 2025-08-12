35fe3bc13c0df14438b9dc26fc942ed8a8794ee7
정남진 <randomuser14123@yu.ac.kr>
2025-08-12 13:43:23 +0900

    feat/implement getting a notification by its id in NotificationService
    Jira : S13P11D103-385

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/service/NotificationService.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/service/NotificationService.kt
@@ -2,11 +2,24 @@ package com.example.amr_backend.v1.service
 
 import com.example.amr_backend.v1.entity.Notification
 import com.example.amr_backend.v1.repository.NotificationRepository
+import com.example.amr_backend.v1.repository.getNotificationById
+import jakarta.persistence.EntityNotFoundException
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class NotificationService(
     private val notificationRepository: NotificationRepository,
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     fun findAll(): List<Notification> = notificationRepository.findAll()
+
+    fun getById(id: Long): Notification = try {
+        notificationRepository.getNotificationById(id)
+    } catch (e: EntityNotFoundException) {
+        logger.error("no such notification whose id is {}", id)
+        logger.error(e.stackTraceToString())
+        throw e
+    }
 }
