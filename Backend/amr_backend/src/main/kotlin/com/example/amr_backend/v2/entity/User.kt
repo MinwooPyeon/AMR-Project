b8ef976dcfb3073db5dfb140443e5f673c6b7d68
정남진 <randomuser14123@yu.ac.kr>
2025-08-13 15:48:12 +0900

    fix/rename user table to avoid name duplication

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v2/entity/User.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v2/entity/User.kt
@@ -8,7 +8,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "user")
+@Table(name = "_user")
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
