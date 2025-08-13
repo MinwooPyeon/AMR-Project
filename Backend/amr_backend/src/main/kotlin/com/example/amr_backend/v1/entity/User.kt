631a062688e945e1a31ef0b87942d3875511e53c
정남진 <randomuser14123@yu.ac.kr>
2025-08-13 13:01:31 +0900

    feat/define user entity
    Jira : S13P11D103-397

---
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/entity/User.kt
@@ -0,0 +1,21 @@
+package com.example.amr_backend.v1.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "user")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long = 0L,
+    @Column(name = "username", unique = true)
+    var username: String,
+    @Column(name = "password")
+    var password: String,
+)
