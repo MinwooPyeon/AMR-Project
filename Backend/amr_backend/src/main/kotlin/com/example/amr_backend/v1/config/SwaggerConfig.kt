8f72b434d2b987b9c19bd605e8e167d02de5138a
정남진 <randomuser14123@yu.ac.kr>
2025-08-14 16:14:28 +0900

    feat/config swagger bean
    Jira : S13P11D103-420

---
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/config/SwaggerConfig.kt
@@ -0,0 +1,22 @@
+package com.example.amr_backend.v1.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .components(Components())
+            .info(
+                Info()
+                    .title("AMR API")
+                    .description("API for AMR project")
+                    .version("1.0.0")
+            )
+    }
+}
