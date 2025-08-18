e288b86a02a84ae0ce70a8f825ee4b4969e0da30
정남진 <randomuser14123@yu.ac.kr>
2025-08-18 09:34:01 +0900

    feat/deduplicate fcm tokens

--- Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/fcm/FirebaseMessageHandler.kt
+++ Backend/amr_backend/src/main/kotlin/com/example/amr_backend/v1/fcm/FirebaseMessageHandler.kt
@@ -7,15 +7,16 @@ import com.fasterxml.jackson.module.kotlin.convertValue
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class FirebaseMessageHandler(
     private val firebaseMessaging: FirebaseMessaging,
-    fcmTokenRepository: FcmTokenRepository,
+    private val fcmTokenRepository: FcmTokenRepository,
     private val objectMapper: ObjectMapper,
 ) {
-    private val tokens = fcmTokenRepository.findAll()
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun sendAlertMessage(message: AlertOutboundMessage) {
         val title = message.title
@@ -27,7 +28,11 @@ class FirebaseMessageHandler(
     private fun <T : Any> T.toPayload(): Map<String, String> = objectMapper.convertValue(this)
 
     private fun send(title: String, body: String, payload: Map<String, String>) {
-        val messages = tokens.map {
+        val tokens = fcmTokenRepository.findAll()
+        val tokenSet = tokens.map { it.token }.toSet()
+        logger.debug("tokens in set are {}", tokenSet)
+
+        val messages = tokenSet.map {
             val notification = Notification.builder()
                 .setTitle(title)
                 .setBody(body)
@@ -36,7 +41,7 @@ class FirebaseMessageHandler(
             Message.builder()
                 .setNotification(notification)
                 .putAllData(payload)
-                .setToken(it.token)
+                .setToken(it)
                 .build()
         }
 
